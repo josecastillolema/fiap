9d1132a5430f49f53113ccaabe3130eb99def9d1
José Castillo Lema <josecastillolema@gmail.com>
2021-09-10 11:48:45 -0300

    Create main.tf

---
+++ cld/multicloud/lab14-iaas-terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-087c17d1fe0178315"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "fiap-vm"
+  }
+}
