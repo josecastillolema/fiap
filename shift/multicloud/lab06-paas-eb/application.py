e5a3c222c252df8c876613d664d28cceb12104b4
Filipe Abreu <filabreu@gmail.com>
2022-06-15 21:57:46 -0300

    Update application.py

--- shift/multicloud/lab06-paas-eb/application.py
+++ shift/multicloud/lab06-paas-eb/application.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<h1>Hola FIAP!</h1>\nMBA! o/"
+    return "<h1>Hola FIAP!</h1>\nMBA! o/ v2"
 
 if __name__ == '__main__':
     # application.run()
